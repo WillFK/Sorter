02a831a5528d280702e7d87f33e4b019f828362e
William <williamferk@gmail.com>
2017-10-29 21:55:17 +0100

    Initial Commit

---
+++ Sorter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sorter
+//
+//  Created by William Koehler on 29.10.17.
+//  Copyright © 2017 William Koehler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
