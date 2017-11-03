f4e555b056909d4eb86aa85756ad41ab48fd10e4
William <williamferk@gmail.com>
2017-11-03 22:51:31 +0100

    quicksort

--- Sorter/main.swift
+++ Sorter/main.swift
@@ -8,8 +8,26 @@
 
 import Foundation
 
-func bubbleSort( subject: inout [UInt32]) {
-    
+func check(subject: inout [UInt32]) {
+    var last = subject[0]
+    var sorted = true
+    for index in 1..<subject.count {
+        if (subject[index] < last) {
+            sorted = false
+            break
+        }
+        else {
+            last = subject[index]
+        }
+    }
+    if sorted {
+        print("sorted")
+    } else {
+        print("not sorted")
+    }
+}
+
+func bubbleSort(subject: inout [UInt32]) {
     print("bubble sorting...")
     for iteration in (1..<subject.count-1).reversed() {
         for index in (0...iteration) {
@@ -17,13 +35,43 @@ func bubbleSort( subject: inout [UInt32]) {
                 let temp = subject[index]
                 subject[index] = subject[index+1]
                 subject[index+1] = temp
-                print("bubble sorting: \(subject)")
+                //print("bubble sorting: \(subject)")
             }
         }
+    }
+    print("bubble sorted: \(subject)")
+}
+
+func quickSort(subject: inout [UInt32]) {
+    print("init quicksort: \(subject)")
+    _quickSort(subject: &subject, start: 0, end: subject.count-1)
+}
+
+func _quickSort(subject: inout [UInt32], start: Int, end: Int) {
+    if end > start {
+        var pos = start
+        for index in (start+1...end) {
+            print("quick sorting - comparing : \(subject[start]) X \(subject[index])")
+            if subject[index] < subject[start] {
+                pos = pos+1
+                let temp = subject[index]
+                subject[index] = subject[pos]
+                subject[pos] = temp
+                print("quick sorting: \(subject)")
+            }
+        }
+        let temp = subject[start]
+        subject[start] = subject[pos]
+        subject[pos] = temp
+        print("quick sorting: \(subject)")
+        _quickSort(subject: &subject, start: start, end: pos-1)
+        _quickSort(subject: &subject, start: pos+1, end: end)
     }
 }
 
 print("getting started...")
 var subject = (1...10).map{_ in arc4random() % 50}
 print("subject: \(subject)")
-bubbleSort(subject: &subject)
+check(subject: &subject)
+quickSort(subject: &subject)
+check(subject: &subject)
