87a5730d240b90c621b54ce300e1d06f04615402
William <williamferk@gmail.com>
2017-11-05 10:16:59 +0100

    general improvements and bubblesort fix

--- Sorter/main.swift
+++ Sorter/main.swift
@@ -8,7 +8,17 @@
 
 import Foundation
 
-func check(subject: inout [Int]) {
+func getSubjects() -> [[Int]] {
+    var subjects = [[Int]]()
+    subjects.append([Int](0...9))
+    subjects.append([Int](0...9).reversed())
+    for _ in (0...18) {
+        subjects.append((0...(9 + arc4random() % 5)).map{_ in Int(arc4random() % 50)})
+    }
+    return subjects
+}
+
+func check(subject: inout [Int]) -> Bool {
     var last = subject[0]
     var sorted = true
     for index in 1..<subject.count {
@@ -25,29 +35,30 @@ func check(subject: inout [Int]) {
     } else {
         print("not sorted")
     }
+    return sorted
 }
 
-func bubblesort(subject: inout [UInt32]) {
-    print("bubble sorting...")
-    for iteration in (1..<subject.count-1).reversed() {
+func bubblesort(subject: inout [Int]) {
+    print("bubblesorting \(subject)")
+    for iteration in (0..<subject.count-1).reversed() {
         for index in (0...iteration) {
             if (subject[index] > subject[index+1]) {
                 let temp = subject[index]
                 subject[index] = subject[index+1]
                 subject[index+1] = temp
-                //print("bubble sorting: \(subject)")
             }
         }
     }
-    print("bubble sorted: \(subject)")
+    print("bubblesorted: \(subject)")
 }
 
-func quicksort(subject: inout [UInt32]) {
-    print("init quicksort: \(subject)")
+func quicksort(subject: inout [Int]) {
+    print("quicksorting: \(subject)")
     _quicksort(subject: &subject, start: 0, end: subject.count-1)
+    print("quicksorted: \(subject)")
 }
 
-func _quicksort(subject: inout [UInt32], start: Int, end: Int) {
+func _quicksort(subject: inout [Int], start: Int, end: Int) {
     if end > start {
         var pos = start
         for index in (start+1...end) {
@@ -70,47 +81,55 @@ func _quicksort(subject: inout [UInt32], start: Int, end: Int) {
 }
 
 func mergesort(subject: inout [Int]) {
+    print("mergesorting: \(subject)")
     mergesortSplit(subject: &subject, startA: 0, endB: subject.count - 1)
+    print("mergesorted: \(subject)")
 }
 
 func mergesortSplit(subject: inout [Int], startA: Int, endB: Int) {
-    //print("merge sorting - split: \(startA) \(endB)")
     if (endB-startA) > 0 {
         let split = (endB + startA) / 2
         mergesortSplit(subject: &subject, startA: startA, endB: split)
         mergesortSplit(subject: &subject, startA: split +  1, endB: endB)
         mergesortJoin(subject: &subject, startA: startA, endA: split, endB: endB)
-        //print("merge sorting: \(subject)")
     }
 }
 
 func mergesortJoin(subject: inout [Int], startA: Int, endA: Int, endB: Int) {
-    //print("merge sorting - joining: \(startA) \(endA) \(endB)")
     var indexA = startA
     var indexB = endA + 1
     let partial = subject[startA...endB]
-    print("merge sorting - partial: \(partial)")
     for index in (startA...endB) {
         if indexB > endB || (endA >= indexA && partial[indexA] < partial[indexB]) {
-            //print("merge sorting - check: \(indexB > endB) \(endA > indexA) \(partial[indexA] < partial[indexB])  \(partial[indexA])  \(partial[indexB])")
-            //print("merge sorting - joining A: \(partial[indexA]) \(index)")
             subject[index] = partial[indexA]
             indexA = indexA + 1
         } else {
-            //print("merge sorting - check: \(indexB > endB) \(endA > indexA) \(subject[indexA] < subject[indexB])  \(subject[indexA])  \(subject[indexB])")
-            //print("merge sorting - joining B: \(partial[indexB]) \(index)")
             subject[index] = partial[indexB]
             indexB = indexB + 1
         }
-        //print("merge sorting - partial: \(partial)")
-        print("merge sorting - subject: \(subject)")
     }
 }
 
 print("getting started...")
-var subject = (1...(10 + arc4random() % 11)).map{_ in Int(arc4random() % 50)}
-//var subject = [47, 16, 0, 7, 14, 46, 43, 8]
-print("subject: \(subject)")
-check(subject: &subject)
-mergesort(subject: &subject)
-check(subject: &subject)
+let subjects = getSubjects()
+for subject in subjects {
+    var copy = subject
+    bubblesort(subject: &copy)
+    if !check(subject: &copy) {
+        break
+    }
+}
+for subject in subjects {
+    var copy = subject
+    quicksort(subject: &copy)
+    if !check(subject: &copy) {
+        break
+    }
+}
+for subject in subjects {
+    var copy = subject
+    mergesort(subject: &copy)
+    if !check(subject: &copy) {
+        break
+    }
+}
